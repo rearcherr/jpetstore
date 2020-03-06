584d8b073cc40db560e1008d4101ccd7368ef967
17308405962 <dolo.liu@qq.com>
2020-03-06 09:04:09 +0800

    framework&readme

---
+++ src/main/java/com/demo/demo/JpetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpetstoreApplication.class, args);
+    }
+
+}
